65b9749afae874b612c59ecdac379f25c4d7f130
shahadkgm <raseena313@kgm.com>
2025-03-22 16:22:30 +0530

    last change

--- config/db.js
+++ config/db.js
@@ -1,13 +1,15 @@
-const mongoose=require("mongoose");
-const env=require("dotenv").config();
-const connectDB= async()=>{
-    try {
-      await  mongoose.connect(process.env.MONGODB_URI)
-      console.log("db connected succsesfully")
-    } catch (error) {
-        
-        console.error("DB connection error:", error); process.exit(1);
-    }
+const mongoose = require("mongoose");
+const env = require("dotenv").config();
+const connectDB = async () => {
+  try {
+    const mongo = await mongoose.connect(process.env.MONGODB_URI)
+    console.log(mongo.connection.name)
+    console.log(mongo.connection.host)
+    console.log("db connected succsesfully")
+  } catch (error) {
+
+    console.error("DB connection error:", error); process.exit(1);
+  }
 
 }
-module.exports=connectDB;
+module.exports = connectDB;  
